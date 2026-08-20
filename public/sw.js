219b98775b0972062d5aae7b0166485559664e05
Michael N Mwanza <mwanza.n.m@gmail.com>
2026-08-20 17:08:47 +0200

    pwa added

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'zamsam-v1';
+const ASSETS = [
+  '/',
+  '/favicon.ico',
+  '/manifest.json',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
